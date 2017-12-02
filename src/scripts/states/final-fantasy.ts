c4e1e3e7bcdc3b25db42b5a4e31e53c3a1aba134
Maria Castro <mariacastro87@gmail.com>
2017-12-02 19:40:08 -0300

    Set battle music

--- src/scripts/states/final-fantasy.ts
+++ src/scripts/states/final-fantasy.ts
@@ -8,6 +8,7 @@ const caveImage = require('assets/images/ffiv/Cave.gif')
 const mistDragonImage = require('assets/images/ffiv/MistDragon1.gif')
 const cecilImage = require('assets/images/ffiv/Cecil1.gif')
 const kainImage = require('assets/images/ffiv/Kain.gif')
+const battleMusic = require('assets/sound/ffiv/bossfight.mp3')
 
 export default class FinalFantasyState extends State {
 
@@ -16,16 +17,20 @@ export default class FinalFantasyState extends State {
   cecil: Phaser.Sprite
   kain: Phaser.Sprite
   party: Phaser.Sprite[]
+  music: Phaser.Sound
 
   preload(): void {
     this.game.load.image('cave', caveImage)
     this.game.load.image('mistDragon', mistDragonImage)
     this.game.load.image('cecil', cecilImage)
     this.game.load.image('kain', kainImage)
+    this.game.load.audio('bossBattleTheme', battleMusic)
   }
   
   create(): void {
     this.party = []
+    this.music = this.game.add.sound('bossBattleTheme', 1)
+    this.music.play('', 0.3)
     this.caveBackground = this.game.add.sprite(0, 0, 'cave')     
     this.caveBackground.scale.setTo(1.6)
     this.caveBackground.smoothed = false
@@ -35,6 +40,7 @@ export default class FinalFantasyState extends State {
     console.log(this.caveBackground.width)
     this.setMistDragon()
     this.setParty(['kain', 'cecil'])
+   
   }
 
   update(): void {
