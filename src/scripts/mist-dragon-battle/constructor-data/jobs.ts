e5251dcd7156da4f3381673e5bdb7b8784782b3a
Maria Castro <mariacastro87@gmail.com>
2017-12-09 02:54:28 -0300

    Update assets

---
+++ src/scripts/mist-dragon-battle/constructor-data/jobs.ts
@@ -0,0 +1,158 @@
+'use strict'
+import { COMMANDS, CHARACTER_STATUS } from './../constants';
+
+import Character from '../elements/character'
+import Boss from '../elements/boss'
+import { STATUS_CODES } from 'http';
+
+interface IAction {
+  executor: string,
+  idExec: number,
+  idTarget: number,
+  idAction: number
+}
+
+interface IATBData {
+  newATB: number,
+  returnAction?: IAction
+}
+
+export const DRAGOON = {
+  name: 'Dragoon',
+  specialAttack: {
+    key: 'jump',
+    name: 'Jump',
+    chargeTime: 5,
+    jumpTarget: {},
+    hitAnimation: Phaser.Sprite,
+    async perform(character: Character, target: Boss): Promise<Boolean> {
+      let promise: Promise<Boolean>
+      if (character.status === CHARACTER_STATUS.JUMP) {
+        promise = this.finishJump(character)
+      } else {
+        await character.goToFront()    
+        promise = this.startJump(character, target)    
+      }
+      return promise
+    },
+ 
+    startJump(character: Character, target: Boss): Promise<Boolean> {
+      return new Promise(resolve => {
+        const timer: Phaser.Timer = character.sprite.game.time.create(false)
+        character.sprite.loadTexture(character.atlasKey, 'weak')
+        timer.loop(Phaser.Timer.QUARTER, () => {
+          character.sprite.loadTexture(character.atlasKey, 'item')
+          const tween = character.sprite.game.add.tween(character.sprite).to({ y: character.sprite.y - 200, x: character.sprite.x - 50 }, 80, "Linear", true)
+          timer.stop()
+          character.setStatus(CHARACTER_STATUS.JUMP)
+          this.jumpTarget = target
+          resolve(true)
+          timer.destroy()
+        })
+        timer.start()
+      })
+      
+    },
+
+    finishJump(character: Character) {
+      return new Promise(resolve => {
+        character.sprite.position.set(20, 50)
+        character.sprite.loadTexture(character.sprite.key, 'jump')
+        this.hitAnimation = character.game.add.sprite(100, 100, 'jump')
+        this.hitAnimation.scale.setTo(1.6)
+        this.hitAnimation.animations.add('start')                
+        const hitTween = character.sprite.game.add.tween(character.sprite).to({ y: this.jumpTarget.sprite.centerY, x: this.jumpTarget.sprite.centerX }, 200, "Linear")
+        const returnTween = character.sprite.game.add.tween(character.sprite).to({
+          x: [400, character.initialPosition.x],
+          y: [10, character.initialPosition.y]
+        }, 300);
+        returnTween.interpolation(function (v, k) {
+          return Phaser.Math.bezierInterpolation(v, k);
+        })
+        returnTween.onComplete.add(() => {
+          this.hitAnimation.animations.play('start', 70)
+          character.ATB = 0
+          character.status = CHARACTER_STATUS.NORMAL
+          character.resetPosition()
+          resolve(true)
+        }, this, 1, character)
+        hitTween.chain(returnTween)
+        hitTween.start()
+      })
+      
+    } 
+  },
+
+  fillATB(character: Character): IATBData {
+    const ATBData = <any>{}
+    if (character.status === CHARACTER_STATUS.JUMP) {
+      ATBData.newATB = character.ATB + this.specialAttack.chargeTime > 100 ? 100 : character.ATB + this.specialAttack.chargeTime
+      if (ATBData.newATB === 100) {
+        ATBData.returnAction = {
+          executor: 'CHARACTER',
+          idExec: character.id,
+          idTarget: this.specialAttack.jumpTarget.id,
+          idAction: COMMANDS.SPECIAL_ATTACK.ID
+        }
+      }
+    } else {
+      ATBData.newATB = character.ATB + character.stats.SPEED > 100 ? 100 : character.ATB + character.stats.SPEED      
+    }
+    return ATBData
+  }
+}
+
+export const DARK_KNIGHT = {
+  name: 'Dark Knight',
+  specialAttack: {
+    key: 'darkness',
+    name: 'Darkness',
+    emmiter: Phaser.EMITTER,
+    async perform (character: Character, target: Boss) {
+      return new Promise(resolve => {
+        const timer = character.game.time.create(false)
+        let ready: boolean = false
+        character.sprite.loadTexture(character.sprite.key, 'dark1')
+        timer.loop(Phaser.Timer.HALF + Phaser.Timer.QUARTER, () => {
+          if (!ready) {
+            character.sprite.loadTexture(character.sprite.key, 'dark2')      
+            ready = true              
+          } else {
+            timer.stop()            
+            this.emitParticles(character).then((value) => {
+              character.resetPosition()
+              resolve(value)
+            })
+          }
+        })
+        timer.start()
+      })
+    },
+
+    emitParticles(character: Character) {
+      return new Promise (resolve => {
+        this.emitter = character.game.add.emitter(character.sprite.x - 70, character.game.world.centerY, 20)
+        this.emitter.makeParticles('dark')
+        this.emitter.forEach((singleParticle) => {
+          singleParticle.animations.add('start', [11, 12, 13, 14, 15, 16, 15, 14, 13, 12, 11])
+          singleParticle.animations.play('start', 25, true, true)
+        }, this)
+        this.emitter.setRotation(0, 90);
+        this.emitter.setScale(0.5, 1, 0.5, 1, 6000, Phaser.Easing.Linear.None);
+        this.emitter.gravity.set(-1000, -200)
+        this.emitter.start(false, 3000, 1);
+        character.game.time.events.add(2000, () => {
+          this.emitter.destroy()
+          resolve(true)
+        }, this);
+      })  
+    }
+  },
+
+  fillATB(character: Character): IATBData {
+    const ATBData = <any>{}
+    ATBData.newATB = character.ATB + character.stats.SPEED > 100 ? 100 : character.ATB + character.stats.SPEED      
+    return ATBData
+  }
+
+}
---
+++ src/scripts/types-definitions/phaser-demos.d.ts
@@ -0,0 +1,163 @@
+interface IPosition {
+  x: number,
+  y: number
+}
+
+declare namespace BattleMenu {
+  interface Cursor {
+    sprite: Phaser.Sprite,
+    currentOption: number
+  }
+
+  interface MenuPoint {
+    x: number,
+    y: number
+  }
+
+  interface MenuSize {
+    width: number,
+    height: number
+  }
+
+  interface BackgroundConfig {
+    position: MenuPoint,
+    anchor: MenuPoint,
+    size: MenuSize
+  }
+
+  interface CharacterMenuInfo {
+    id: number,
+    name: Phaser.Text,
+    healthInfo: Phaser.Text
+    initiativeBar?: Phaser.Sprite //no idea yet
+  }
+
+  interface EnemyMenuInfo {
+    id: number,
+    name: Phaser.Text
+  }
+
+  interface CommandsMenuInfo {
+    id: number,
+    name: Phaser.Text,
+    position: string,
+    cursorPosition?: MenuPoint
+  }
+
+  interface CharactersMenuSection {
+    background: Phaser.Sprite,
+    charactersList: CharacterMenuInfo[]
+  }
+
+  interface EnemiesMenuSection {
+    background: Phaser.Sprite,
+    enemiesList: EnemyMenuInfo[]
+  }
+
+  interface CommandsMenuSection {
+    background: Phaser.Sprite,
+    commandsList: CommandsMenuInfo[]
+  }
+
+  interface CharacterData {
+    id: number,
+    name: string,
+    specialAttack: string,
+    items?: string[]
+    totalHealth?: number,
+    remainingHealth?: number,
+    cursorPosition?: MenuPoint
+  }
+
+
+  interface EnemyData {
+    id: number,
+    name: string,
+    cursorPosition?: MenuPoint
+  }
+
+  interface MenuData {
+    characters: CharacterData[],
+    enemies: EnemyData[]
+  }
+}
+
+declare namespace CharacterAction {
+  interface ActionData {
+    executor: string,
+    idExec: number,
+    idTarget: number,
+    idAction: number
+  }
+
+  interface ReadyCharacter {
+    idReady: number,
+    automaticAction?: ActionData
+  }
+}
+
+declare namespace Character {
+
+  interface Base {
+    game: Phaser.Game
+    id: number
+    atlasKey: string
+    name: string
+    level: number
+    status: number
+    sprite: Phaser.Sprite
+    ATB: number
+    stats: Stats
+    job: Job.Base
+    animations: Animations
+    initialPosition: IPosition
+    tintTimer: Phaser.Timer
+  }
+
+  interface Stats {
+    HP: number,
+    MP: number,
+    STRENGTH: number,
+    SPEED: number,
+    STAMINA: number,
+    INTELLECT: number,
+    SPIRIT: number
+  }
+
+  interface AnimationData {
+    animation?: Phaser.Animation,
+    hitAnimation?: Phaser.Sprite
+    play?(onEndCallback?: Function)
+  }
+
+  interface Animations {
+    walk: AnimationData,
+    attack?: AnimationData,
+    victory?: AnimationData
+  }
+}
+
+declare namespace Job {
+  interface Base {
+    specialAttack: SpecialAttack
+    fillATB: Function
+    performSpecialAttack?: Function
+  }
+
+  interface SpecialAttack {
+    key: string,
+    name: string,
+    chargeTime?: number,
+    perform?: Function
+  }
+}
+
+
+
+
+
+
+
+
+
+
